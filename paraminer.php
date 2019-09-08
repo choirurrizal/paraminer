0ffd88cf5466d961b29295b9e0f4d491994869cc
Choirur Rizal <choirurrizal321@gmail.com>
2019-09-09 04:29:38 +0700

    Update paraminer.php

--- paraminer.php
+++ paraminer.php
@@ -49,6 +49,7 @@ class paraminer
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36");
+		curl_setopt($ch, CURLOPT_TIMEOUT, 600);
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, array($_param => $_value));
 		$sourceCode = curl_exec($ch);
@@ -69,6 +70,7 @@ class paraminer
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36");
+		curl_setopt($ch, CURLOPT_TIMEOUT, 600);
 		$sourceCode = curl_exec($ch);
 		curl_close($ch);
 		
